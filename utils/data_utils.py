1bde4774bc5467df9abb4d059a8e8ce4e4bedaad
tqth <tqthinh2611@gmail.com>
2026-03-22 14:06:18 +0700

    add

---
+++ utils/data_utils.py
@@ -0,0 +1,82 @@
+import logging
+import os
+import torch
+
+from torchvision import transforms, datasets
+from torch.utils.data import DataLoader, RandomSampler, DistributedSampler, SequentialSampler
+
+
+logger = logging.getLogger(__name__)
+
+
+def get_loader(args):
+    if args.local_rank not in [-1, 0]:
+        torch.distributed.barrier()
+
+    transform_train = transforms.Compose([
+        transforms.RandomResizedCrop((args.img_size, args.img_size), scale=(0.05, 1.0)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+    ])
+    transform_test = transforms.Compose([
+        transforms.Resize((args.img_size, args.img_size)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+    ])
+
+    if args.dataset == "cifar10":
+        trainset = datasets.CIFAR10(root="./data",
+                                    train=True,
+                                    download=True,
+                                    transform=transform_train)
+        testset = datasets.CIFAR10(root="./data",
+                                   train=False,
+                                   download=True,
+                                   transform=transform_test) if args.local_rank in [-1, 0] else None
+
+    elif args.dataset == "cifar100":
+        trainset = datasets.CIFAR100(root="./data",
+                                     train=True,
+                                     download=True,
+                                     transform=transform_train)
+        testset = datasets.CIFAR100(root="./data",
+                                    train=False,
+                                    download=True,
+                                    transform=transform_test) if args.local_rank in [-1, 0] else None
+    elif args.dataset == "custom":     # ================== CUSTOM DATASET ==================
+        train_dir = os.path.join(args.data_path, "train")
+        val_dir   = os.path.join(args.data_path, "val")
+
+        trainset = datasets.ImageFolder(
+            root=train_dir,
+            transform=transform_train
+        )
+
+        testset = datasets.ImageFolder(
+            root=val_dir,
+            transform=transform_test
+        ) if args.local_rank in [-1, 0] else None
+
+        num_classes = len(trainset.classes)
+        logger.info(f"Number of classes: {num_classes}")
+        logger.info(f"Classes: {trainset.classes}")
+    else:
+        raise ValueError("Unsupported dataset")
+
+    if args.local_rank == 0:
+        torch.distributed.barrier()
+
+    train_sampler = RandomSampler(trainset) if args.local_rank == -1 else DistributedSampler(trainset)
+    test_sampler = SequentialSampler(testset)
+    train_loader = DataLoader(trainset,
+                              sampler=train_sampler,
+                              batch_size=args.train_batch_size,
+                              num_workers=4,
+                              pin_memory=True)
+    test_loader = DataLoader(testset,
+                             sampler=test_sampler,
+                             batch_size=args.eval_batch_size,
+                             num_workers=4,
+                             pin_memory=True) if testset is not None else None
+
+    return train_loader, test_loader
